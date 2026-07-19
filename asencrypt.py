ca9a966dce0d8a6efea4413c74a528f3024f3799
Andrew Taylor <rolyata47@gmail.com>
2026-07-19 01:14:55 -0500

    Simplifying code

--- asencrypt.py
+++ asencrypt.py
@@ -4,7 +4,7 @@ from addsubtract import encrypt
 def main():
     argparser = argparse.ArgumentParser(prog='asencrypt.py', description='Encrypt a message using the addsubtract algorithm')
     group = argparser.add_mutually_exclusive_group(required=True)
-    group.add_argument('-m', '--message', type=str, nargs='?')
+    group.add_argument('-m', '--message', type=str)
     group.add_argument('-i', '--inputfile', type=str)
     argparser.add_argument('-k', '--key', type=float, default=10e5)
     argparser.add_argument('-o', '--outputfile', type=str)
